501af417245ad41f992c3a5a5d13b04a1b1d585d
DimaMarkow <dimamarkow@gmail.com>
2023-09-09 17:10:17 +0300

    app start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import Header from 'components/Header/Header';
+
+class App extends Component {
+  state = {
+    showFeedback: false,
+  };
+
+  // showFeedback = () => {
+  //   this.setState(state => ({ showFeedback: !state.showFeedback }));
+  // };
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Header showFeedback={this.showFeedback} />
+          {/* {this.state.showFeedback && <Feedback />} */}
+        </div>
+        <ToastContainer
+          position="top-left"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </>
+    );
+  }
+}
+
+export default App;
